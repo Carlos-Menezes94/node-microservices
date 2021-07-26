aad1b3453d7be2d9406b355e698e5de63f8e84fb
Carlos-Menezes94 <83547045+Carlos-Menezes94@users.noreply.github.com>
2021-07-25 21:19:50 -0300

    expressJS configuration, consign

---
+++ config/config.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var consign = require('consign');
+var bodyParser = require('body-parser');
+var app = express();
+ 
+  
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+ 
+ consign().include("./config/connection.js")
+    .then("app/repositorys")
+    .then("app/controllers")
+    .into(app);
+ 
+ 
+module.exports = app;
